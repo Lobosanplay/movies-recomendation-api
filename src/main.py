1b20e7b508ab440215029cd824ac286b4ed9df0d
Lobosanplay <parra.santiago2008@gmail.com>
2026-04-21 12:06:30 -0500

    fix: change .txt to .toml with uv and refactor.

--- src/main.py
+++ src/main.py
@@ -1,24 +1,31 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
-from services.model_service import recommendation_service
-from api.v1.router import api_router
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from api.v1.router import api_router
+from services.model_service import recommendation_service
 
-@app.on_event("startup")
-async def startup_event():
+
+@asynccontextmanager
+async def startup_event(app: FastAPI):
     """Se ejecuta al iniciar la aplicación"""
     await recommendation_service.initialize()
+    yield
+
+
+app = FastAPI(lifespan=startup_event)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],          
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
-    allow_methods=["*"], 
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix="/api/v1")
+
 
 @app.get("/")
 async def root():
@@ -28,5 +35,11 @@ async def root():
         "version": "1.0.0",
         "docs": "/docs",
         "redoc": "/redoc",
-        "health": "/api/v1/health"
+        "health": "/api/v1/health",
     }
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", reload=True)
