220cd2c2890c07fa220529482f8a5779c2f4c336
Lobosanplay <parra.santiago2008@gmail.com>
2026-04-22 10:24:10 -0500

    shore: origins

--- src/main.py
+++ src/main.py
@@ -18,7 +18,10 @@ app = FastAPI(lifespan=startup_event)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173/",
+        "https://movies-recomends-page.vercel.app/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
